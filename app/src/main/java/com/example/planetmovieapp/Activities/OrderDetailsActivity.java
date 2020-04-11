262a5c1142bea6f7df08656eca189ffe87278055
Chen <chen_y@ti.com>
2020-04-11 18:38:29 +0300

    Get statistics Activity

--- app/src/main/java/com/example/planetmovieapp/Activities/OrderDetailsActivity.java
+++ app/src/main/java/com/example/planetmovieapp/Activities/OrderDetailsActivity.java
@@ -36,4 +36,4 @@ public class OrderDetailsActivity extends AppCompatActivity {
 
     }
 
-}
+
---
+++ app/src/main/java/com/example/planetmovieapp/AdministrationActivities/ShowStatisticsResult.java
@@ -0,0 +1,31 @@
+package com.example.planetmovieapp.AdministrationActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.planetmovieapp.R;
+
+public class ShowStatisticsResult extends AppCompatActivity {
+    String movie_stat;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_statistics_result);
+        textView = findViewById(R.id.statistics_result);
+        try {
+            Intent intent = getIntent();
+            movie_stat = (String) intent.getSerializableExtra("SELLING_STATISTICS");
+            textView.setText(movie_stat);
+            Log.d("kok","koka!!" + movie_stat);
+
+        }catch (Exception e){
+            Log.d("kokk","koka!!");
+        }
+    }
+}
